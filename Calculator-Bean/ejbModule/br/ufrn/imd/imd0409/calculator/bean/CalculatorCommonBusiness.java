97cc5992c0bfe09adfe913d6de763780eece506a
Carlos Eduardo da Silva <kaduardo@gmail.com>
2015-10-01 15:20:20 -0300

    the Bean project and its interfaces

---
+++ Calculator-Bean/ejbModule/br/ufrn/imd/imd0409/calculator/bean/CalculatorCommonBusiness.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package br.ufrn.imd.imd0409.calculator.bean;
+
+/**
+ * @author kaduardo
+ *
+ */
+public interface CalculatorCommonBusiness {
+	public int add(int... arguments);
+}
---
+++ Calculator-Bean/ejbModule/br/ufrn/imd/imd0409/calculator/bean/CalculatorLocalBusiness.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package br.ufrn.imd.imd0409.calculator.bean;
+
+/**
+ * @author kaduardo
+ *
+ */
+public interface CalculatorLocalBusiness extends CalculatorCommonBusiness {
+
+}
